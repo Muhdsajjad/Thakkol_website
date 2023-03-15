58b0379e7285bcce9fce8310fe108a7f721b269b
rishadpk <106809583+rishadpk759@users.noreply.github.com>
2023-03-15 11:57:06 +0530

    whatsapp button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,7 +232,7 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
+      typeSpeed: 50,
       backSpeed: 50,
       backDelay: 2000
     });
@@ -311,6 +311,8 @@
    * Initiate Pure Counter 
    */
     new PureCounter();
+
+    
 
   })()
 
